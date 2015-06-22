1fbfc0d0f4c90ebc712a49d6206b3d33d87c8453
Francisco Madrid <fjmadrid@gmail.com>
2015-06-22 10:32:57 -0300

    fixing indentation

--- restriccion_scl/__init__.py
+++ restriccion_scl/__init__.py
@@ -4,8 +4,8 @@ import os
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 config_file_path = os.environ.get(
-	'RESTRICCION_SCL_CONFIG',
-	os.path.join(script_path, '..', 'configs/localhost.json')
+    'RESTRICCION_SCL_CONFIG',
+    os.path.join(script_path, '..', 'configs/localhost.json')
 )
 
 config_file = open(config_file_path, 'r')
