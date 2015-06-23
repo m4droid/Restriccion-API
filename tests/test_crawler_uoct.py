1a74b5f943b5ce8e1377b892495caa792586c748
Francisco Madrid <fjmadrid@gmail.com>
2015-06-22 21:50:52 -0300

    improving crawler tests

--- tests/test_crawler_uoct.py
+++ tests/test_crawler_uoct.py
@@ -1,5 +1,6 @@
 from mock import patch
 import moment
+from pyquery import PyQuery as pq
 
 from .base_tests import BaseTestCase
 from restriccion_scl.crawlers.uoct import UOCT_Crawler
@@ -7,17 +8,17 @@ from restriccion_scl.crawlers.uoct import UOCT_Crawler
 
 class TestUoct_Crawler(BaseTestCase):
 
-    def test_clean_digits_string_ok(self):
+    def test_crawler_uoct_clean_digits_string_ok(self):
         self.assertEqual('1-2-3', UOCT_Crawler.clean_digits_string('1-2-3'))
 
-    def test_clean_digits_string_none_value(self):
+    def test_crawler_uoct_clean_digits_string_none_value(self):
         self.assertIsNone(UOCT_Crawler.clean_digits_string(None))
 
-    def test_clean_digits_string_multiple_dash(self):
+    def test_crawler_uoct_clean_digits_string_multiple_dash(self):
         self.assertEqual('1-2-3', UOCT_Crawler.clean_digits_string('1-2--3'))
         self.assertEqual('1-2-3', UOCT_Crawler.clean_digits_string('1---2-3'))
 
-    def test_clean_digits_string_ends_dashes(self):
+    def test_crawler_uoct_clean_digits_string_ends_dashes(self):
         # Single
         self.assertEqual('1-2-3', UOCT_Crawler.clean_digits_string('1-2-3-'))
         self.assertEqual('1-2-3', UOCT_Crawler.clean_digits_string('-1-2-3'))
@@ -28,10 +29,10 @@ class TestUoct_Crawler(BaseTestCase):
         self.assertEqual('1-2-3', UOCT_Crawler.clean_digits_string('--1-2-3'))
         self.assertEqual('1-2-3', UOCT_Crawler.clean_digits_string('--1-2-3--'))
 
-    def test_clean_digits_string_order(self):
+    def test_crawler_uoct_clean_digits_string_order(self):
         self.assertEqual('1-2-3', UOCT_Crawler.clean_digits_string('2-1-3'))
 
-    def test_parse(self):
+    def test_crawler_uoct_parse_file(self):
         crawler = UOCT_Crawler()
         crawler.url = self.get_fixture_file_path('uoct.cl_restriccion-vehicular_0.html')
 
@@ -41,8 +42,22 @@ class TestUoct_Crawler(BaseTestCase):
         self.assertEqual(26, len(new_restrictions))
         self.assertEqual(26, self.mongo_db.restrictions.count())
 
+    @patch('restriccion_scl.crawlers.uoct.pq')
+    def test_crawler_uoct_parse_url(self, mock_pyquery):
+        mock_pyquery.side_effect = lambda **kw: pq(
+            filename=self.get_fixture_file_path('uoct.cl_restriccion-vehicular_1.html').replace('file://', '')
+        )
+
+        crawler = UOCT_Crawler()
+
+        self.assertEqual(0, self.mongo_db.restrictions.count())
+        new_restrictions = crawler.parse()
+        self.assertEqual(list, type(new_restrictions))
+        self.assertEqual(27, len(new_restrictions))
+        self.assertEqual(27, self.mongo_db.restrictions.count())
+
     @patch('restriccion_scl.crawlers.uoct.moment.now')
-    def test_database_entry_data(self, mock_moment):
+    def test_crawler_uoct_data_integrity(self, mock_moment):
         mock_moment.side_effect = lambda: moment.date('2015-06-22', '%Y-%m-%d')
 
         crawler = UOCT_Crawler()
@@ -64,7 +79,7 @@ class TestUoct_Crawler(BaseTestCase):
         )
 
     @patch('restriccion_scl.crawlers.uoct.moment.now')
-    def test_parse_new_entry(self, mock_moment):
+    def test_crawler_uoct_parse_new_entry(self, mock_moment):
         mock_moment.side_effect = lambda: moment.date('2015-06-22', '%Y-%m-%d')
 
         crawler = UOCT_Crawler()
@@ -106,7 +121,7 @@ class TestUoct_Crawler(BaseTestCase):
         )
 
     @patch('restriccion_scl.crawlers.uoct.moment.now')
-    def test_parse_update_entry(self, mock_moment):
+    def test_crawler_uoct_parse_update_entry(self, mock_moment):
         mock_moment.side_effect = lambda: moment.date('2015-06-22T00:00:00', '%Y-%m-%dT%H:%M:%S')
 
         first_entries = []
