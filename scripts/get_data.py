3ccea6635ad89f3f2825d171b765c26c183e4996
Francisco Madrid <fjmadrid@gmail.com>
2015-06-25 05:32:44 -0300

    fixing get data script

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
-from restriccion_scl.crawlers.uoct import UOCT_Crawler
+import pymongo
 
+from restriccion_scl import CONFIG
+from restriccion_scl.crawlers.uoct import UOCT_Crawler
+from restriccion_scl.models.restriction import Restriction
+
+
+mongo_client = pymongo.MongoClient(**CONFIG['pymongo']['client'])
+mongo_db = mongo_client[CONFIG['pymongo']['database']]
 
 crawler = UOCT_Crawler()
-crawler.parse()
+restrictions = crawler.parse()
+
+Restriction.insert_many(mongo_db, restrictions)
+
+mongo_client.close()
