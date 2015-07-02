61ed50acf34e695963a42ba4f63181a116ed6116
Francisco Madrid <fjmadrid@gmail.com>
2015-07-02 18:51:06 -0300

    notify to devices on new data

--- scripts/get_data_uoct.py
+++ scripts/get_data_uoct.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import sys
+
 import pymongo
 
 from restriccion_scl import CONFIG
 from restriccion_scl.crawlers.uoct import UOCT_Crawler
+from restriccion_scl.models.device import Device
 from restriccion_scl.models.restriction import Restriction
 
 
@@ -13,5 +16,10 @@ crawler = UOCT_Crawler()
 restrictions = crawler.parse()
 
 Restriction.insert_many(mongo_db, restrictions)
+
+if '--notify' in sys.argv[1:]:
+	data = restrictions[0]
+	data['tipo'] = 'restricciones'
+	Device.notify(mongo_db, data, collapse_key='restricciones')
 
 mongo_client.close()
